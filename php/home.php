5de654ac04cd4a21815abccb682e39a4812b1841
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-04-15 16:33:31 -0500

    agg cerrar sesion

---
+++ php/home.php
@@ -0,0 +1,25 @@
+<?php 
+session_start();
+
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.html");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-sacale=1.0">
+        <title>Control Streaming</title>
+        <link rel="stylesheet" href="styles/styles.css"> 
+    </head>
+    <body>
+        <header>
+            <a href="logout.php">Cerrar sesión</a>
+            <h1>Sistema de cuentas de Streaming</h1>
+            <h2>Bienvenido, <?php echo $_SESSION['usuario']; ?> 🎉</h2>
+        </header>
+    </body>
+</html>
