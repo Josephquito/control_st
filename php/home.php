d186cb4ff982651da4caa2bdea526e716634a0c1
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-04-15 22:21:33 -0500

    agg cerrar sesion

--- php/home.php
+++ php/home.php
@@ -13,7 +13,7 @@ if (!isset($_SESSION['usuario'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-sacale=1.0">
         <title>Control Streaming</title>
-        <link rel="stylesheet" href="styles/styles.css"> 
+        <link rel="stylesheet" href="/app.jotavix.com/styles/styles.css"> 
     </head>
     <body>
         <header>
