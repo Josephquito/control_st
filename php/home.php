ba874eabb96904a23946baf5b0fad5e54dcbf58e
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-04-16 11:37:38 -0500

    crear y enlazar sidebar en las pag

--- php/home.php
+++ php/home.php
@@ -16,48 +16,10 @@ if (!isset($_SESSION['usuario'])) {
         <link rel="stylesheet" href="/styles/styles.css"> 
     </head>
     <body class="home-body">
-        <div class="sidebar" id="sidebar">
-            <div class="sidebar-header">
-                <h2><?php echo $_SESSION['usuario']; ?></h2>
-            </div>
-        <nav class="menu">
-            <a class="nav-link" href="#">
-                <img src="../assets/inicio.png" alt="Inicio" class="icon"> 
-                <span class="text-label">Inicio</span>
-                <span class="tooltip-text">Inicio</spann>
-            </a>
-            <a class="nav-link" href="#" data-colapsar="true">
-                <img src="../assets/cuentas.png" alt="Cuentas" class="icon"> 
-                <span class="text-label">Cuentas</span>
-                <span class="tooltip-text">Cuentas</span>
-            </a>
-            <a class="nav-link" href="#">
-                <img src="../assets/notificaciones.png" alt="Notificaciones" class="icon"> 
-                <span class="text-label">Notificaciones</span>
-                <span class="tooltip-text">Notificaciones</span>
-            </a>
-            <a class="nav-link" href="#" data-colapsar="true">
-                <img src="../assets/clientes.png" alt="Clientes" class="icon"> 
-                <span class="text-label">Clientes</span>
-                <span class="tooltip-text">Clientes</span>
-            </a>
-            <a class="nav-link" href="#">
-                <img src="../assets/ganancias.png" alt="Ganancias" class="icon"> 
-                <span class="text-label">Ganancias</span>
-                <span class="tooltip-text">Ganancias</span>
-            </a>
-        </nav>
-            <div class="nav-link dropdown-toggle" id="btn-mas">
-                <img src="../assets/menu.png" alt="Menu" class="icon"> 
-                <span class="text-label">Más</span>
-                <span class="tooltip-text">Más</span>
-
-                <div class="dropdown-menu" id="menu-mas">
-                    <a href="#" class="dropdown-item">Mi Perfil</a>
-                    <a href="../php/logout.php" class="dropdown-item">Cerrar sesión</a>
-                </div>
-            </div>
-        </div>
+        <?php include('sidebar.php'); ?>
+        <main class="contenido">
+        <h1>Clientes</h1>
+        </main>
         <script src="../scripts/scripts.js"></script>
     </body>
 </html>
---
+++ php/sidebar.php
@@ -0,0 +1,42 @@
+<div class="sidebar" id="sidebar">
+            <div class="sidebar-header">
+                <h2><?php echo $_SESSION['usuario']; ?></h2>
+            </div>
+        <nav class="menu">
+            <a class="nav-link" href="#">
+                <img src="../assets/inicio.png" alt="Inicio" class="icon"> 
+                <span class="text-label">Inicio</span>
+                <span class="tooltip-text">Inicio</spann>
+            </a>
+            <a class="nav-link" href="#" data-colapsar="true">
+                <img src="../assets/cuentas.png" alt="Cuentas" class="icon"> 
+                <span class="text-label">Cuentas</span>
+                <span class="tooltip-text">Cuentas</span>
+            </a>
+            <a class="nav-link" href="#">
+                <img src="../assets/notificaciones.png" alt="Notificaciones" class="icon"> 
+                <span class="text-label">Notificaciones</span>
+                <span class="tooltip-text">Notificaciones</span>
+            </a>
+            <a class="nav-link" href="#" data-colapsar="true">
+                <img src="../assets/clientes.png" alt="Clientes" class="icon"> 
+                <span class="text-label">Clientes</span>
+                <span class="tooltip-text">Clientes</span>
+            </a>
+            <a class="nav-link" href="#">
+                <img src="../assets/ganancias.png" alt="Ganancias" class="icon"> 
+                <span class="text-label">Ganancias</span>
+                <span class="tooltip-text">Ganancias</span>
+            </a>
+        </nav>
+            <div class="nav-link dropdown-toggle" id="btn-mas">
+                <img src="../assets/menu.png" alt="Menu" class="icon"> 
+                <span class="text-label">Más</span>
+                <span class="tooltip-text">Más</span>
+
+                <div class="dropdown-menu" id="menu-mas">
+                    <a href="#" class="dropdown-item">Mi Perfil</a>
+                    <a href="../php/logout.php" class="dropdown-item">Cerrar sesión</a>
+                </div>
+            </div>
+        </div>
