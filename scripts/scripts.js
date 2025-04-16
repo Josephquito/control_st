e3ad2604068ed3375cb290c67a5d4fdeb3a4b53d
Josephquito <jdquito.3@sudamericano.edu.ec>
2025-04-16 11:50:30 -0500

    crear y enlazar sidebar en las pag

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -32,3 +32,14 @@ btnMas.addEventListener('click', (e) => {
 document.addEventListener('click', ()=> {
     menuMas.style.display = 'none';
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const sidebar = document.getElementById('sidebar');
+    const estado = localStorage.getItem('sidebarEstado');
+
+    if (estado === 'colapsada') {
+        sidebar.classList.add('colapsada');
+    } else {
+        sidebar.classList.remove('colapsada');
+    }
+});
